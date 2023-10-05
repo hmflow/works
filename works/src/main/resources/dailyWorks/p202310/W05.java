efbe3d051b4037e38955822eeeebd3e4d71f7495
hmflo <hmflo@host.docker.internal>
2023-10-05 17:19:40 +0900

    20231005

--- works/src/main/resources/dailyWorks/p202310/W05.java
+++ works/src/main/resources/dailyWorks/p202310/W05.java
@@ -1,7 +1,15 @@
 package dailyWorks.p202310;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class W05 {
@@ -24,9 +32,19 @@ public class W05 {
 		}
 	}
 	
+	// 파일을 스트림으로 읽어오기
+	public void dataSearch() {
+		try (Stream<String> readData = new BufferedReader(new InputStreamReader(new FileInputStream("file/c2022Data.csv"),"euc-kr")).lines()){
+			Map<Object, List<String[]>> mapData = readData.map(lineStr->lineStr.split(",")).collect(Collectors.groupingBy(linArr->linArr[3]));
+			mapData.keySet().forEach(System.err::println);			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}  
+	}
+	
 	
 	public static void main(String[] args) {
-		new W05().findFileForder("c://");
-		
+		//new W05().findFileForder("c://");
+		new W05().dataSearch();
 	}
 }
