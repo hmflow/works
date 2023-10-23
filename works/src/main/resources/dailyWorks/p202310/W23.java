8220c6191c00dba39e82caef76cd89fe00c21ca8
hmflo <hmflo@DESKTOP-K15SESQ>
2023-10-23 20:52:08 +0900

    연습 COMMIT

---
+++ works/src/main/resources/dailyWorks/p202310/W23.java
@@ -0,0 +1,62 @@
+package dailyWorks.p202310;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class W23 {
+	public W23(){
+	} 
+	
+	public void fileSearch(String fileName) {
+		try(Stream<File> fileStram = Arrays.stream(new File(fileName).listFiles())){
+			Map<Boolean, List<File>> fMap = fileStram.collect(Collectors.groupingBy(f->f.isFile()));
+			fMap.get(true).forEach(System.out::println);
+			fMap.get(false).forEach(f->{
+				fileSearch(f.toString());
+			});
+		}
+	}
+	
+	public void useClipbard() {
+		Clipboard cp = Toolkit.getDefaultToolkit().getSystemClipboard();
+		try {
+			String cpTxt = (String) cp.getData(DataFlavor.stringFlavor);
+			System.err.println(cpTxt);
+			String saveStr = String.format("copyed-- %S", cpTxt);
+			cp.setContents(new StringSelection(saveStr), null);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void callFile() {
+		String filePath = "file/c2022Data.csv";
+		try {
+			FileInputStream fis = new FileInputStream(filePath);
+			InputStreamReader isr = new InputStreamReader(fis,"euc-kr");
+			BufferedReader br = new BufferedReader(isr);
+			Stream<String> stream = br.lines();
+			stream.map(line -> line.split(",")).forEach(line ->{
+				System.err.println(line[0]);
+			});
+		}catch(Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	public static void main(String[] args) { 
+		//new W23().fileSearch("c:\\");
+		new W23().callFile();
+	}
+}
