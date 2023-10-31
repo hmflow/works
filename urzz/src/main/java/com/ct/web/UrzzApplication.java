8a47da9655d3416a035f48d5af4d85c3483d39a0
hmflo <hmflo@DESKTOP-K15SESQ>
2023-10-31 16:56:53 +0900

    ㅎㅎ

--- urzz/src/main/java/com/ct/web/UrzzApplication.java
+++ urzz/src/main/java/com/ct/web/UrzzApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UrzzApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UrzzApplication.class, args);
 	}
