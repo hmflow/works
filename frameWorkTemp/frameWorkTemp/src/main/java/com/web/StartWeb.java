3b8ee79ea064d6d63ee9a100a0674852bf464437
hmflo <hmflo@DESKTOP-K15SESQ>
2023-11-24 15:56:38 +0900

    아.. 저장된 로그인정보가 안뜨넹..

--- frameWorkTemp/frameWorkTemp/src/main/java/com/web/StartWeb.java
+++ frameWorkTemp/frameWorkTemp/src/main/java/com/web/StartWeb.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.web.mapper")
 public class StartWeb {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) { 
 		SpringApplication.run(StartWeb.class, args);
 	}
 }
