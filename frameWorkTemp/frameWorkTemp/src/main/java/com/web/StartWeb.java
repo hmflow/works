c6ce2a103545db3f6c103a91a2131f0aec36b242
hmflo <hmflo@DESKTOP-K15SESQ>
2023-11-24 15:54:57 +0900

    안수정

--- frameWorkTemp/frameWorkTemp/src/main/java/com/web/StartWeb.java
+++ frameWorkTemp/frameWorkTemp/src/main/java/com/web/StartWeb.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.web.mapper")
 public class StartWeb {
 	public static void main(String[] args) {
+		
 		SpringApplication.run(StartWeb.class, args);
 	}
 }
