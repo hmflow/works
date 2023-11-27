29aba990cc5f70b52cbba8fa90c21f0ad48144b7
JAVAWANG <hmflow@gmail.com>
2023-11-27 14:29:36 +0900

    Delete wangIsGogack directory

--- wangIsGogack/wang/src/main/java/com/web/mapperImpls/mappers/SiminMapper.java
+++
@@ -1,6 +0,0 @@
-package com.web.mapperImpls.mappers;
-import org.apache.ibatis.annotations.Mapper;
-import com.web.mapper.MybatisMapper;
-// CreateMybatisFile 클래스로 부터 생성되어진 파일입니다.
-@Mapper
-public abstract class SiminMapper implements MybatisMapper {}
